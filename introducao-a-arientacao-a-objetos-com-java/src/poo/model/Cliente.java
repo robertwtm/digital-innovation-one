633ff151dd3dd4adeb9ddadb607ea91ed6df815b
robertwtm <robertwtavares@gmail.com>
2021-11-18 00:59:18 -0300

    editando as pastas

---
+++ introducao-a-arientacao-a-objetos-com-java/src/poo/model/Cliente.java
@@ -0,0 +1,12 @@
+package poo.model;
+
+public class Cliente {
+	
+	private int codigo;
+	private String cpf;
+	private String nome;
+
+	
+	
+	
+}
---
+++ introducao-a-arientacao-a-objetos-com-java/src/poo/model/Endereco.java
@@ -0,0 +1,12 @@
+package poo.model;
+
+public class Endereco {
+	
+	private String endereço;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String estado;
+	private String cep;
+	
+}
