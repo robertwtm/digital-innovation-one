8d5358bcd36730981978aea24cb901d8b4015e14
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-11-26 00:32:41 -0300

    Update ParIMpar.java

--- desafios-java-developer/src/ParImpar/ParIMpar.java
+++ desafios-java-developer/src/ParImpar/ParIMpar.java
@@ -1,3 +1,4 @@
+package ParImpar;
 // DESAFIO: Leia um valor inteiro N. Este valor será a quantidade de valores que serão lidos em seguida. Para cada valor lido, mostre uma mensagem em inglês dizendo se este valor lido é par (EVEN), ímpar (ODD), positivo (POSITIVE) ou negativo (NEGATIVE). No caso do valor ser igual a zero (0), embora a descrição correta seja (EVEN NULL), pois por definição zero é par, seu programa deverá imprimir apenas NULL.
 // Saída: Para cada caso, imprima uma mensagem correspondente, de acordo com o exemplo abaixo. Todas as letras deverão ser maiúsculas e sempre deverá haver um espaço entre duas palavras impressas na mesma linha.
 
