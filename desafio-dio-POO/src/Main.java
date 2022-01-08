011f31d791dde67c0dca55021e7e4e260eb0ac58
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2022-01-08 20:01:33 -0300

    Mentoria e Curso add Class

---
+++ desafio-dio-POO/src/Main.java
@@ -0,0 +1,30 @@
+import me.dio.desafio.dominio.Curso;
+import me.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("mentoria JAva");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
+}
